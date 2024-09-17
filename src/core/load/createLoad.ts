bab3239ed3103a0b312b5fa8c36bb782a89d0d5d
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-09-17 18:08:47 +0800

    Add documentation for different overloads of `createLoad`

--- src/core/load/createLoad.ts
+++ src/core/load/createLoad.ts
@@ -1,10 +1,14 @@
 import { LoadSignal } from './LoadSignal.js';
-import { pool } from '../pool/pool.js';
+import { pool as globalPool } from '../pool/pool.js';
 
-export function createLoad(context = pool) {
+/** Creates a load onto the provided context */
+export function createLoad(pool: typeof globalPool): LoadSignal;
+/** Creates a load onto the global context */
+export function createLoad(): LoadSignal;
+export function createLoad(pool = globalPool) {
 	const signal = new LoadSignal();
 
-	context.update((signals) => [...signals, signal]);
+	pool.update((signals) => [...signals, signal]);
 
 	return signal;
 }
