a7f38ea6f663d03b420720a78b272e4790bf82ba
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-09-17 17:02:46 +0800

    Revert allowing project `.ts` imports
    * This caused vscode to prefer `.ts` to `.js` imports during autocomplete

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,11 +1,12 @@
 import { mergeConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// change this back to .js when the issue is fixed
-// https://github.com/vitest-dev/vitest/issues/6519
-import config from './vite.config.ts';
-
-// vitest.workspace.ts
 import { defineWorkspace } from 'vitest/config';
+// change this back to .js when the issue is fixed
+// @ts-expect-error https://github.com/vitest-dev/vitest/issues/6519
+import configTs from './vite.config.ts';
+import type configJs from './vite.config.js';
+
+const config: typeof configJs = configTs;
 
 export default defineWorkspace([
 	mergeConfig(config, {
