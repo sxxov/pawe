227840f043f9b883bbde440543a44c63513877e3
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-09-17 16:43:17 +0800

    Include `finish` as an intrinsic method for all `ReadableLoadSignal`s

--- src/core/load/signal/ReadableLoadSignal.ts
+++ src/core/load/signal/ReadableLoadSignal.ts
@@ -1,5 +1,7 @@
 import { type ReadableSignal } from '../../../utils/signal/ReadableSignal.js';
 import { type LoadProgressScalar } from './LoadProgressScalar.js';
 
-export type ReadableLoadSignal = {} & ReadableSignal<LoadProgressScalar> &
+export type ReadableLoadSignal = {
+	finish(): void;
+} & ReadableSignal<LoadProgressScalar> &
 	PromiseLike<void>;
