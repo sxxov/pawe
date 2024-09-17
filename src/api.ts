5815596d5901c567a3fc367b81291153cee7bd27
jason lim <50891910+Sxxov@users.noreply.github.com>
2024-09-17 17:40:07 +0800

    Expose `writeDOM`

--- src/api.ts
+++ src/api.ts
@@ -2,12 +2,16 @@ export { createLoad } from './core/load/createLoad.js';
 export { LoadSignal } from './core/load/LoadSignal.js';
 export { type ReadableLoadSignal } from './core/load/ReadableLoadSignal.js';
 export { type LoadScalar } from './core/load/LoadScalar.js';
+
 export { bar } from './core/loader/bar/bar.js';
 export { createBar } from './core/loader/bar/createBar.js';
 export { progress } from './core/loader/progress/progress.js';
 export { createProgress } from './core/loader/progress/createProgress.js';
 export { pool } from './core/pool/pool.js';
 export { createPool } from './core/pool/createPool.js';
+
 export { monitorDOM } from './dom/sources/monitorDOM.js';
 export { monitorFetch } from './dom/sources/monitorFetch.js';
 export { monitorXMLHttpRequest } from './dom/sources/monitorXMLHttpRequest.js';
+
+export { writeDOM } from './dom/expose/writeDOM.js';
